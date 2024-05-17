91eded469039fa321395f29bada2e446bdaf0dcc
Elena R <74355341+elenrobe@users.noreply.github.com>
2024-05-17 18:57:08 +0200

    otro cambio pequeño xd

--- src/ImgPreProcessorBW.cpp
+++ src/ImgPreProcessorBW.cpp
@@ -12,7 +12,12 @@ cv::Mat ImgPreProcessorBW::processImage(Mat image)
 	source = image;
 	cv::resize(source, source, Size(910, 512));
 
-	cvtColor(source, source, COLOR_BGR2GRAY);
+	if (image.channels() != 1) {
+		// If the image has only one channel, it is grayscale
+		cvtColor(source, source, COLOR_BGR2GRAY);
+
+	}
+
 
 	cvtColor(source, destino, COLOR_GRAY2BGR);
 
